2f4d2bf42080112b39646a1b8f188da809fa4cfe
acerspeedracer <ace6598@gmail.com>
2013-04-06 20:04:00 -0400

    added python support

--- app.py
+++ app.py
@@ -2,11 +2,17 @@ from flask import Flask
 from werkzeug.contrib.fixers import ProxyFix
 from flask import render_template
 from flask import request
+import pymongo
+from pymongo import MongoClient
+
 app = Flask(__name__)
+client = MongoClient('localhost',9003)
 
 @app.route("/")
 def email():
   return "apples"
+
+@app.route(
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
