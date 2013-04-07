e36175d1e5cb786ef3b08cb57a0608a71344441b
Aiser Shiekh <aiser@localhost.(none)>
2013-04-06 21:04:56 -0700

    fixed a bunch of stuff woot

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, redirect, url_for, render_template
+from werkzeug import secure_filename
+import os
 from werkzeug.contrib.fixers import ProxyFix
 import pymongo
 from pymongo import MongoClient
@@ -8,38 +10,49 @@ client = MongoClient('localhost',9003)
 
 @app.route("/")
 def email():
-  return "apple pie"
+	return "apple pie"
 
 @app.route("/<username>")
 def create_user_page (username):
-  db = client.launchpad
-  allUsers = db.users
-  user = allUsers.find_one({"user":username})
-  if user == None:
-    newUser = {"user":username,"test":"New person woot"}
-    allUsers.insert(newUser)
-    user = allUsers.find_one({"user":username})
-  test = user["test"]
-  return test
+	db = client.launchpad
+	allUsers = db.users
+	user = allUsers.find_one({"user":username})
+	if user == None:
+		newUser = {"user":username,"test":"New person woot"}
+		allUsers.insert(newUser)
+		user = allUsers.find_one({"user":username})
+	test = user["test"]
+	return test
 
-UPLOAD_FOLDER = "/<username>/songs"
 ALLOWED_EXTENSIONS = set(['ogg', 'wav', 'mp3'])
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
 	return '.' in filename and \
 			filename.rsplit('.',1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/<username>/upload/<filename>', methods = ['GET', 'POST'])
-def upload_file(username, filename):
-	if request.method == 'POST':
-		file = request.files['file']
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			return redirect(url_for('uploaded_file', username = username, filename = filename))
+@app.route('/<username>/upload', methods = ['GET', 'POST'])
+def upload_file(username):
+	try :
+		if request.method == 'POST':
+			filelist = request.files.getlist('files')
+			for f in filelist :
+				if f and allowed_file(f.filename):
+					filename = secure_filename(f.filename)
+					f.save(os.path.join('public/%s/'%username, filename))
+					db = client.launchpad
+					allUsers = db.users
+					user = allUsers.find_one({"user":username})
+					if user == None:
+						return "Not a user"
+					fnd = allUsers.find_one({"user":username,"songs":{"$elemMatch":{"file":filename}}})
+					if fnd != None:
+						continue
+					song = {"file":filename, "loc":'public/%s/%s'%(username,filename)}
+					allUsers.update({"user":username},{"$push": {"songs":song}})
+	except Exception as e:
+		return "%s"%str(e)
+	return render_template('test.html',username=username)
 
 @app.route('/<username>/songs/<filename>')
 def uploaded_file(username, filename):
@@ -47,9 +60,8 @@ def uploaded_file(username, filename):
 
 @app.route('/<username>/uploadthings')
 def uploadthings(username):
-  return render_template(test.html)
+	return render_template('test.html',username=username)
 
-app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == "__main__":
-  app.run(debug=True)
+	app.run(debug=True)
